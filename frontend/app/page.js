7e064455cb8d595805f845183185d36a2ea90233
dayumsam <dayumsam22@gmail.com>
2023-09-17 01:36:00 +0000

    added frontend

---
+++ frontend/app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className=' m-auto max-w-sm h-[720px] border-2 border-black '>
+      hello
+    </div>
+  )
+}
